d3e117508db61739f298f9304a376b41249e60c5
徐怡 <xy6609@ly.com>
2016-10-18 13:59:39 +0800

    添加IDbConfig接口

---
+++ MyUtils/DBData/IDbConfig.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyUtils.DBData
+{
+    internal interface IDbConfig
+    {
+        /// <summary>
+        /// 数据库连接配置
+        /// </summary>
+        void DbConfig();
+
+        /// <summary>
+        /// 新增
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        int Add<T>(T entity);
+
+        /// <summary>
+        /// 修改
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        int Update<T>(T entity);
+
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        int Delete(int id);
+
+        /// <summary>
+        /// 查询列表
+        /// </summary>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        List<T> SelectList<T>(NameValueCollection param);
+
+        /// <summary>
+        /// 查询实体
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        T SelectEntity<T>(int id);
+    }
+}
---
+++ MyUtils/DBData/SqlData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyUtils.DBData
+{
+    public class SqlData : IDbConfig
+    {
+        public void DbConfig()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Add<T>(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update<T>(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T SelectEntity<T>(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> SelectList<T>(NameValueCollection param)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
