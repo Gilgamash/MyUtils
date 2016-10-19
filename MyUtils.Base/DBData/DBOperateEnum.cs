38a9fdbe62de7531afdf5d45a6c618ba827fe161
徐怡 <xy6609@ly.com>
2016-10-19 13:43:48 +0800

    sqldata编写

---
+++ MyUtils.Base/DBData/DBOperateEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyUtils.Base.DBData
+{
+    public enum DBOperateEnum
+    {
+        Insert = 1,
+        Update = 2,
+        Delete = 3,
+        Select = 4
+    }
+}
--- MyUtils/DBData/SqlData.cs
+++ MyUtils/DBData/SqlData.cs
@@ -4,19 +4,31 @@ using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using MyUtils.Base.DBData;
 
 namespace MyUtils.DBData
 {
     public class SqlData : IDbConfig
     {
+        private string dbConnectStr;
+
         public void DbConfig()
         {
-            throw new NotImplementedException();
+            dbConnectStr = ConfigurationManager.ConnectionStrings[""].ConnectionString;
         }
 
         public int Add<T>(T entity)
         {
-            throw new NotImplementedException();
+            int id = 0;
+            SqlCommand cmd = new SqlCommand();
+            using (SqlConnection conn = new SqlConnection(dbConnectStr))
+            {
+
+            }
+            return id;
         }
 
         public int Update<T>(T entity)
@@ -37,6 +49,32 @@ namespace MyUtils.DBData
         public List<T> SelectList<T>(NameValueCollection param)
         {
             throw new NotImplementedException();
+        }
+
+        private string ConvertSql<T>(T entity, DBOperateEnum operate)
+        {
+            string sql = string.Empty;
+            switch (operate)
+            {
+                case DBOperateEnum.Insert:
+                    break;
+                case DBOperateEnum.Update:
+                    break;
+                case DBOperateEnum.Delete:
+                    break;
+                case DBOperateEnum.Select:
+                    break;
+                default:
+                    break;
+            }
+            return sql;
+        }
+
+        private string InsertSql<T>(T entity)
+        {
+            string sql = string.Empty;
+
+            return sql;
         }
     }
 }
